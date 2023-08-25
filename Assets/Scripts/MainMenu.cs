cb7206c4758344efa2faac79f37269d24e0b9cf0
CAfeaki <k.afeaki.16@gmail.com>
2023-08-25 16:17:49 +1000

    Update 28/05.4
    - Updated credit panel to show on press
    - Added item UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject creditsPanel;
+
     public void PlayButton()
     {
         SceneManager.LoadScene("Scene1");
+    }
+
+    public void CreditsButton()
+    {
+        if (creditsPanel.activeSelf)
+        {
+            creditsPanel.SetActive(false);
+        }
+        else
+        {
+            creditsPanel.SetActive(true);
+        }
     }
 
     public void QuitButton()
